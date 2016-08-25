7243e90d27db563d32aec97ede1c496f3323cbd3
Mayank Agrawal <agarwalm214@gmail.com>
2016-08-25 14:21:59 +0530

    Add prodct schema

--- product.js
+++ product.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Category = require('./category');
+var fx = require('./fx');
 
 var productSchema = {
 	name : { type : String , required : true},
@@ -7,17 +8,48 @@ var productSchema = {
 	pictures : [{ type: String, match : /^http:\/\//i }],
 	
 	price : {
-		amount : { type: Number, required : true },
+		amount : { 
+					type: Number,
+				   	required : true,
+				   	set : function(v){
+				   			this.internal.approximatePriceUSD = 
+				   				v/( fx()[this.price.currency] || 1 );
+				   				return v;
+				   } 
+				},
 
 		currency : {
 			type : String,
 			enum : ['INR','USD','EUR'],
-			required : true
+			required : true,
+			set : function(v){
+				this.internal.approximatePriceUSD = 
+					this.price.amount/ ( fx()[v] || 1);
+
+				return v;
+			}
 		}
 	},
 
 	category : Category.categorySchema
 };
 
-module.exports = new mongoose.Schema(productSchema);
-module.exports.productSchema = productSchema;
+// module.exports = new mongoose.Schema(productSchema);
+// module.exports.productSchema = productSchema;
+
+var schema = new mongoose.Schema(productSchema);
+var currencySymbols = {
+	'USD' : '$',
+	'INR' : '₹',
+	'EUR' : '€'
+};
+
+schema.virtual('displayPrice').get(function(){
+	return currencySymbols[this.price.currency] + ' '+
+			this.price.amount;
+});
+
+schema.set('toObject', { virtuals : true});
+schema.set('toJSON', { virtuals : true});
+
+module.exports = schema;
